7b8d0167002d0b59aaf1035d3b14b8e94b70433f
Козлов Александр Евгеньевич <Kozlov.A.Evge@sberbank.ru>
2021-09-02 23:39:03 +0300

    change plugin add kubeapi

--- setup.go
+++ setup.go
@@ -28,37 +28,41 @@ func setup(c *caddy.Controller) error {
 
 	re := New()
 
+	go func() {
+
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+
+		// set up signals so we handle the first shutdown signal gracefully
+		stopCh := signals.SetupSignalHandler()
+
+		exampleClient, err := clientset.NewForConfig(config)
+		if err != nil {
+			klog.Fatalf("Error building example clientset: %s", err.Error())
+		}
+
+		exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
+
+		controller := NewController(exampleClient,
+			exampleInformerFactory.Networking().V1().FederationDNSs())
+
+		// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
+		// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
+		exampleInformerFactory.Start(stopCh)
+
+		if err = controller.Run(2, stopCh); err != nil {
+			klog.Fatalf("Error running controller: %s", err.Error())
+		}
+
+	}()
+
 	// Add the Plugin to CoreDNS, so Servers can use it in their plugin chain.
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
 		re.Next = next
 		return re
 	})
-
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-
-	// set up signals so we handle the first shutdown signal gracefully
-	stopCh := signals.SetupSignalHandler()
-
-	exampleClient, err := clientset.NewForConfig(config)
-	if err != nil {
-		klog.Fatalf("Error building example clientset: %s", err.Error())
-	}
-
-	exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, time.Second*30)
-
-	controller := NewController(exampleClient,
-		exampleInformerFactory.Networking().V1().FederationDNSs())
-
-	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(stopCh)
-	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-	exampleInformerFactory.Start(stopCh)
-
-	if err = controller.Run(2, stopCh); err != nil {
-		klog.Fatalf("Error running controller: %s", err.Error())
-	}
 
 	// All OK, return a nil error.
 	return nil
